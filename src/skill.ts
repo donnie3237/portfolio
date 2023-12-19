473c4c13c187b41b5b4b84c46d94c6803b1d18cf
D0NN1E <paradorn3237@gmail.com>
2023-12-19 18:38:54 +0700

    .

---
+++ src/skill.ts
@@ -0,0 +1,14 @@
+export const skill =[
+    {
+        name : 'Electrical',
+        logo : '/skill/elec.png'
+    },
+    {
+        name : 'Eng',
+        logo : '/skill/en.png'
+    },
+    {
+        name : 'Word',
+        logo : 'word'
+    },
+]
---
+++ src/stack.ts
@@ -0,0 +1,82 @@
+export const stack = [
+    {
+        name: "react",
+        logo: "/stack/react.png"
+    },
+    {
+        name: "Solid",
+        logo: "/stack/solid.png"
+    },
+    {
+        name: "Scss",
+        logo: "/stack/sass.png"
+    },
+    {
+        name: "Next",
+        logo: "/stack/next.png"
+    },
+    {
+        name: "Astro",
+        logo: "/stack/astro.png"
+    },
+    {
+        name: "Tailwind",
+        logo: "/stack/tailwind.png"
+    },
+    {
+        name: "MongoDB",
+        logo: "/stack/mongo.png"
+    },
+    {
+        name: "Prisma",
+        logo: "/stack/prisma.png"
+    },
+    {
+        name: "Rust",
+        logo: "/stack/rust.png"
+    },
+    {
+        name: "PostgreSQL",
+        logo: "/stack/postgre.png"
+    },
+    {
+        name: "Vitest",
+        logo: "/stack/vitest.png"
+    },
+    {
+        name: "Tauri",
+        logo: "/stack/tauri.png"
+    },
+    {
+        name: "k6",
+        logo: "/stack/K6.png"
+    },
+    {
+        name: "Actix-web",
+        logo: "/stack/actix.png"
+    },
+    {
+        name: "Socket.io",
+        logo: "/stack/socket.png"
+    },
+    {
+        name: "express",
+        logo: "/stack/express.png"
+    },
+    {
+        name: "TypeScript",
+        logo: "/stack/typescript.png"
+    },
+    {
+        name: "LuciaAuth",
+        logo: "/stack/lucia.png"
+    },
+    {
+        name: "Swagger",
+        logo: "/stack/swagger.png"
+    },
+    {
+        name: "Mongoose",
+        logo: "/stack/mongoose.png"
+    },
+]
