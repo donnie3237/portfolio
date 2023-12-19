072f47c538a998437d59c662483e965e86674500
D0NN1E <paradorn3237@gmail.com>
2023-12-19 19:28:28 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import solidJs from "@astrojs/solid-js";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), solidJs()]
+  integrations: [tailwind(), react(), solidJs(), compress()]
 });
--- src/skill.ts
+++ src/skill.ts
@@ -9,6 +9,18 @@ export const skill =[
     },
     {
         name : 'Word',
-        logo : 'word'
+        logo : '/skill/word.png'
+    },
+    {
+        name : 'Slidev',
+        logo : '/skill/slidev.png'
+    },
+    {
+        name : 'PowerBI',
+        logo : '/skill/bi.png'
+    },
+    {
+        name : 'Slidev',
+        logo : '/skill/slidev.png'
     },
 ]
