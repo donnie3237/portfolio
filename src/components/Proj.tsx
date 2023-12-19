8b301affea71d656348426b21ec080749fef4fe2
D0NN1E <paradorn3237@gmail.com>
2023-12-19 19:57:00 +0700

    .

--- src/components/Proj.tsx
+++ src/components/Proj.tsx
@@ -1,7 +1,7 @@
 import { project } from "../project"
 import './proj.scss'
-import { Component } from "solid-js";
-export default function Proj({}:any):Component {
+
+export default function Proj({}:any) {
   return (
     <div class="love grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4 w-[100%]">
         {project && project.map((result) => (
