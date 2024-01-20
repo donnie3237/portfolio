68970759407986959f6625aabc9bd574d9e6a77c
D0NN1E <paradorn3237@gmail.com>
2024-01-20 22:32:38 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
-const site = "https://dose3.dxse.site";
+const site = "https://dxse.site";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [tailwind({ applyBaseStyles: false,}), solidJs(), sitemap()]
+  integrations: [tailwind({ applyBaseStyles: false,}), solidJs(), sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
