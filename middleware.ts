4938170560c79fa6c57733a35170660b31153eb5
D0NN1E <paradorn3237@gmail.com>
2024-01-20 23:20:18 +0700

    .

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-export default function middleware(request: any) {
-    const url = new URL(request.url);
-    // You can retrieve IP location or cookies here.
-    if (url.pathname !== '/') {
-      url.pathname = '/';
-    }
-    return Response.redirect(url);
-}
