4b86aea3561c43261b680d1305d37a62c1bb938b
D0NN1E <paradorn3237@gmail.com>
2024-01-20 23:17:39 +0700

    .

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 export default function middleware(request: any) {
     const url = new URL(request.url);
     // You can retrieve IP location or cookies here.
-    if (url.pathname === '/*') {
+    if (url.pathname !== '/') {
       url.pathname = '/';
     }
     return Response.redirect(url);
