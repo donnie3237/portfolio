da4144f80667bd5cac3dcf496ed2962aa4725337
D0NN1E <paradorn3237@gmail.com>
2024-01-26 21:08:49 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,11 @@ import vercel from '@astrojs/vercel/static'
 // https://astro.build/config
 export default defineConfig({
   site,
-  output: 'static',
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   }
-  // }),
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
   integrations: [tailwind({ applyBaseStyles: false,}), solidJs(), sitemap()],
 });
