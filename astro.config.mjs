9e26f55361ebaf7b3d8c7234347ee7e5e2215220
D0NN1E <paradorn3237@gmail.com>
2024-12-03 16:00:10 +0700

    big change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
-const site = "https://dxse.site";
-import vercel from '@astrojs/vercel/serverless'
-
-// https://astro.build/config
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 export default defineConfig({
-  site,
-  integrations: [tailwind({ applyBaseStyles: false,}), solidJs(), sitemap()],
+   vite: {
+    plugins: [tailwindcss()],
+  },
+   site: 'https://yourdomain.com',
+  integrations: [sitemap()]
 });
